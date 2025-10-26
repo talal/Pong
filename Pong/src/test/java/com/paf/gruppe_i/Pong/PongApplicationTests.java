b87ebbbfcb833cf47920d40c6e54c4dbb8168b73
romulus <roman.omarov@stud.th-luebeck.de>
2025-10-26 19:02:58 +0100

    Pong backend setup

---
+++ Pong/src/test/java/com/paf/gruppe_i/Pong/PongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paf.gruppe_i.Pong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
