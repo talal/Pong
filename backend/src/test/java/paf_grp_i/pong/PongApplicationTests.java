709e2dcb11e0864cfe555106923281538477ffe9
Muhammad Talal Anwar <3526562+talal@users.noreply.github.com>
2026-01-02 19:47:22 +0100

    test(backend): use transactional
    so that test data doesn't persist in database.

--- backend/src/test/java/paf_grp_i/pong/PongApplicationTests.java
+++ backend/src/test/java/paf_grp_i/pong/PongApplicationTests.java
@@ -6,17 +6,19 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import paf_grp_i.pong.model.User;
 import paf_grp_i.pong.repository.UserRepository;
 
 @SpringBootTest
+@Transactional
 public class PongApplicationTests {
 
     @Autowired private UserRepository repo;
 
     @Test
-    public void testCreateUser() {
+    public void testUser() {
         String email = "foo@gmx.de";
         User user = new User();
         user.setEmail(email);
@@ -27,7 +29,7 @@ public class PongApplicationTests {
         repo.save(user);
         User foundUser = repo.findByEmail(email);
 
-        // Verify the user exists and game stats are initialized correctly
+        // Verify the user exists and game stats are initialized correctly.
         assertNotNull(foundUser, "User should be found in the database");
         assertEquals(email, foundUser.getEmail());
         assertEquals(0, foundUser.getGamesPlayed(), "New user should have 0 games played");
