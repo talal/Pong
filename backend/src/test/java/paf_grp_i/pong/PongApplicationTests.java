87495950708f01b439585f108adb9dfd0db5f1b5
Muhammad Talal Anwar <3526562+talal@users.noreply.github.com>
2026-01-02 19:33:52 +0100

    test(backend): improve

--- backend/src/test/java/paf_grp_i/pong/PongApplicationTests.java
+++ backend/src/test/java/paf_grp_i/pong/PongApplicationTests.java
@@ -1,6 +1,7 @@
 package paf_grp_i.pong;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,17 +18,19 @@ public class PongApplicationTests {
     @Test
     public void testCreateUser() {
         String email = "foo@gmx.de";
-        String lastName = "Bar";
         User user = new User();
         user.setEmail(email);
         user.setPassword("foopw");
         user.setFirstName("Foo");
-        user.setLastName(lastName);
+        user.setLastName("Bar");
 
         repo.save(user);
         User foundUser = repo.findByEmail(email);
 
-        // do the last names of the found user and the saved user match?
-        assertEquals(foundUser.getLastName(), lastName);
+        // Verify the user exists and game stats are initialized correctly
+        assertNotNull(foundUser, "User should be found in the database");
+        assertEquals(email, foundUser.getEmail());
+        assertEquals(0, foundUser.getGamesPlayed(), "New user should have 0 games played");
+        assertEquals(0, foundUser.getGamesWon(), "New user should have 0 wins");
     }
 }
